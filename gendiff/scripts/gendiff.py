4efe7dc962a611d862aee78d6be94c630cc4a730
Aleksei <jiunov@gmail.com>
2023-12-08 06:12:57 +0600

    1

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.parse_args()
+    # print('test')
+
+
+if __name__ == '__main__':
+    main()
