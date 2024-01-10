e06a27fc9d63c1a97d6b40a10197268f57a18ed3
Aleksei <jiunov@gmail.com>
2024-01-10 12:44:07 +0600

    argparse

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,9 +3,12 @@ import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.parse_args()
-    # print('test')
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+
+    args = parser.parse_args()
+    print(args.accumulate(args.integers))
 
 
 if __name__ == '__main__':
