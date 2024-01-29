22abaa856c8ce3d804b4b906b713083b2a204041
Aleksei <jiunov@gmail.com>
2024-01-29 10:40:26 +0600

    <6

--- gendiff/difference.py
+++ gendiff/difference.py
@@ -5,17 +5,40 @@ def generate_diff(first_file_path, second_file_path):
     file1 = get_data(first_file_path)
     file2 = get_data(second_file_path)
 
-    nodes = ['{']
+    diff = []
     keys = sorted(set(file1.keys()) | set(file2.keys()))
 
     for key in keys:
-        if key not in file1:
-            nodes.append(f'  + {key}: {file2[key]}')
-        elif key not in file2:
-            nodes.append(f'  - {key}: {file1[key]}')
+        if key not in file1.keys():
+            diff.append({
+                'key': key,
+                'type': 'add',
+                'value': file2[key]
+            })
+        elif key not in file2.keys():
+            diff.append({
+                'key': key,
+                'type': 'remove',
+                'value': file1[key]
+            })
+        elif isinstance(file1[key], dict) and isinstance(file2[key], dict):
+            diff.append({
+                'key': key,
+                'type': 'children',
+                'value': generate_diff(file1[key], file2[key])
+            })
         elif file1[key] == file2[key]:
-            nodes.append(f'    {key}: {file2[key]}')
+            diff.append({
+                'key': key,
+                'type': 'same',
+                'value': file1[key]
+            })
         else:
-            nodes.append(f'  - {key}: {file1[key]}')
-            nodes.append(f'  + {key}: {file2[key]}')
-    return '\n'.join(nodes) + '\n}\n'
+            diff.append({
+                'key': key,
+                'type': 'change',
+                'value': file1[key],
+                'new_value': file2[key]
+            })
+
+    return diff
