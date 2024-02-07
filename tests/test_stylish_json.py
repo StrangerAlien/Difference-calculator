c3682a0861589a2b5519e79ef9d780420055bd34
Aleksei <jiunov@gmail.com>
2024-02-08 04:24:18 +0600

    7

--- tests/test_stylish_json.py
+++ tests/test_stylish_json.py
@@ -9,15 +9,11 @@ file4 = './tests/fixtures/file4.json'
 
 def test_stylish_json():
     sample_1_2 = './tests/fixtures/stylish_1_2.txt'
-
     with open(sample_1_2) as sample:
         sample_content = sample.read()
-
     assert generate_diff(file1, file2, 'stylish') == sample_content
 
     sample_3_4 = './tests/fixtures/stylish_3_4.txt'
-
     with open(sample_3_4) as sample:
         sample_content = sample.read()
-
     assert generate_diff(file3, file4, 'stylish') == sample_content
--- tests/test_stylish_yml.py
+++ tests/test_stylish_yml.py
@@ -6,7 +6,6 @@ file2 = './tests/fixtures/file2.yml'
 
 def test_stylish_json():
     sample = './tests/fixtures/stylish_1_2.txt'
-
     with open(sample) as sample:
         sample_content = sample.read()
     assert generate_diff(file1, file2, 'stylish') == sample_content
