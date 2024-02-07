a5ac54ae5fe6751afca4c8f941bd16a6ff82c881
Aleksei <jiunov@gmail.com>
2024-02-08 04:55:29 +0600

    7

--- gendiff/formatting/plain.py
+++ gendiff/formatting/plain.py
@@ -1,33 +1,30 @@
 from gendiff.make_str import make_str
 
 
-def plain(diff):
-    def iterate(tree, path):
-        result = []
-        for node in tree:
+def plain(diff, path=''):
+    result = []
+    for node in diff:
 
-            key, type = node.get('key'), node.get('type')
-            value, new_value = node.get('value'), node.get('new_value')
-            value_str = deep_line(value)
-            new_value_str = deep_line(new_value)
+        key, type = node.get('key'), node.get('type')
+        value, new_value = node.get('value'), node.get('new_value')
+        value_str = deep_line(value)
+        new_value_str = deep_line(new_value)
 
-            new_path = key if path == '' else f'{path}.{key}'
-            added = f"Property '{new_path}' was added with value: {value_str}"
-            removed = f"Property '{new_path}' was removed"
-            changed = f"Property '{new_path}' was updated. " \
-                      + f"From {value_str} to {new_value_str}"
+        new_path = key if path == '' else f'{path}.{key}'
+        added = f"Property '{new_path}' was added with value: {value_str}"
+        removed = f"Property '{new_path}' was removed"
+        changed = f"Property '{new_path}' was updated. " \
+                  + f"From {value_str} to {new_value_str}"
 
-            if type == 'add':
-                result.append(added)
-            elif type == 'remove':
-                result.append(removed)
-            elif type == 'change':
-                result.append(changed)
-            elif type == 'children':
-                result.append(iterate(value, new_path))
-        return '\n'.join(result)
-
-    return iterate(diff, '')
+        if type == 'add':
+            result.append(added)
+        elif type == 'remove':
+            result.append(removed)
+        elif type == 'change':
+            result.append(changed)
+        elif type == 'children':
+            result.append(plain(value, new_path))
+    return '\n'.join(result)
 
 
 def deep_line(value):
--- gendiff/formatting/stylish.py
+++ gendiff/formatting/stylish.py
@@ -4,42 +4,38 @@ INDENT = ' '
 SPACES_COUNT = 4
 
 
-def stylish(diff):
-    def iterate(tree, depth=0):
+def stylish(diff, depth=0):
+    result = ['{']
+    offset = depth + SPACES_COUNT
+    for node in diff:
 
-        result = ['{']
-        offset = depth + SPACES_COUNT
-        for node in tree:
+        key, type = node.get('key'), node.get('type')
+        value, new_value = node.get('value'), node.get('new_value')
+        value_str = deep_line(value)
+        new_value_str = deep_line(new_value)
 
-            key, type = node.get('key'), node.get('type')
-            value, new_value = node.get('value'), node.get('new_value')
-            value_str = deep_line(value)
-            new_value_str = deep_line(new_value)
+        spaces = INDENT * (offset - 2)
 
-            spaces = INDENT * (offset - 2)
+        added = f'{spaces}+ {key}: {value_str}'
+        removed = f'{spaces}- {key}: {value_str}'
+        samed = f'{spaces}  {key}: {value_str}'
+        changed = f'{spaces}+ {key}: {new_value_str}'
 
-            added = f'{spaces}+ {key}: {value_str}'
-            removed = f'{spaces}- {key}: {value_str}'
-            samed = f'{spaces}  {key}: {value_str}'
-            changed = f'{spaces}+ {key}: {new_value_str}'
+        if type == 'add':
+            result.append(added)
+        elif type == 'remove':
+            result.append(removed)
+        elif type == 'same':
+            result.append(samed)
+        elif type == 'change':
+            result.append(removed)
+            result.append(changed)
+        elif type == 'children':
+            result.append(f'{INDENT * offset}{key}: '
+                          + stylish(value, offset))
 
-            if type == 'add':
-                result.append(added)
-            elif type == 'remove':
-                result.append(removed)
-            elif type == 'same':
-                result.append(samed)
-            elif type == 'change':
-                result.append(removed)
-                result.append(changed)
-            elif type == 'children':
-                result.append(f'{INDENT * offset}{key}: '
-                              + iterate(value, offset))
-
-        result.append(f'{INDENT * depth + "}"}')
-        return '\n'.join(result)
-
-    return iterate(diff)
+    result.append(f'{INDENT * depth + "}"}')
+    return '\n'.join(result)
 
 
 def deep_line(value, depth=0):
