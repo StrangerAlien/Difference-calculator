1f849eb2fa87a9760c3faa691ea83a91f7d36988
Aleksei <jiunov@gmail.com>
2024-02-15 21:57:32 +0600

    pytest

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,17 +1,14 @@
-# import pytest
-# from gendiff.generate_diff import generate_diff
-#
-# file1_yml = './tests/fixtures/file1.yml'
-# file2_yml = './tests/fixtures/file2.yml'
-#
-# file1_json = './tests/fixtures/file1.json'
-# file2_json = './tests/fixtures/file2.json'
-#
-# file3 = './tests/fixtures/file3.json'
-# file4 = './tests/fixtures/file4.json'
-#
-# test_cases = ['yml', 'json']
-#
-# @pytest.mark.parametrize('test_format', test_cases):
-# def test_gendiff(test_format):
-#     pass
+import pytest
+from gendiff.generate_diff import generate_diff
+
+
+@pytest.mark.parametrize(
+    "file1, file2, format_, expected",
+    [
+        ('tests/fixtures/file1.json', 'tests/fixtures/file2.json', "stylish", 'tests/fixtures/result_stylish'),
+        ('tests/fixtures/file1.json', 'tests/fixtures/file2.json', "plain", 'tests/fixtures/result_plain'),
+        ('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml', "plain", 'tests/fixtures/result_plain'),
+        ('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml', "stylish", 'tests/fixtures/result_stylish')]
+    )
+def test_generate_diff(file1, file2, format_, expected):
+    assert generate_diff(file1, file2, format_) == open(expected).read()
