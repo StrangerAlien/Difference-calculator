cbe7c0d1846af176386886d3f6fe9dba212e9e8d
Aleksei <jiunov@gmail.com>
2024-02-15 22:06:58 +0600

    pytest, flake8

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,14 +1,21 @@
 import pytest
 from gendiff.generate_diff import generate_diff
 
+file1_json = 'tests/fixtures/file1.json'
+file2_json = 'tests/fixtures/file2.json'
+file1_yml = 'tests/fixtures/file1.yml'
+file2_yml = 'tests/fixtures/file2.yml'
+result_plain = 'tests/fixtures/result_plain'
+result_stylish = 'tests/fixtures/result_stylish'
+
 
 @pytest.mark.parametrize(
-    "file1, file2, format_, expected",
+    'file1, file2, format_, expected',
     [
-        ('tests/fixtures/file1.json', 'tests/fixtures/file2.json', "stylish", 'tests/fixtures/result_stylish'),
-        ('tests/fixtures/file1.json', 'tests/fixtures/file2.json', "plain", 'tests/fixtures/result_plain'),
-        ('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml', "plain", 'tests/fixtures/result_plain'),
-        ('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml', "stylish", 'tests/fixtures/result_stylish')]
-    )
+        (file1_json, file2_json, "stylish", 'tests/fixtures/result_stylish'),
+        (file1_json, file2_json, "plain", 'tests/fixtures/result_plain'),
+        (file1_yml, file2_yml, "plain", result_plain),
+        (file1_yml, file2_yml, "stylish", result_stylish)]
+)
 def test_generate_diff(file1, file2, format_, expected):
     assert generate_diff(file1, file2, format_) == open(expected).read()
